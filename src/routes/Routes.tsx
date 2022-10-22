d83ead313afe9d0433b0644ad9baa2284ae43285
Alexander Augusto <alexaasf1010@gmail.com>
2022-10-22 11:09:10 -0300

    Add base url to react router

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -10,7 +10,7 @@ import ChooseEnergy from "../pages/ChooseEnergy"
 
 export default function Router() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/meal" element={<NewMeal />} />
